7361d052eedcd848dec845b180bcb627ba2c59a9
breney <brunopy2@gmail.com>
2020-11-16 15:28:11 +0000

    "Vengere working but in trash code, need to upgrade code"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println(Vigenere.encoder("ola","lindo"));
+        System.out.println(Vigenere.encoder("ola", "lindo"));
+        System.out.println(Vigenere.decoder("ztn", "lindo"));
     }
 }
+
--- src/com/company/Vigenere.java
+++ src/com/company/Vigenere.java
@@ -1,12 +1,15 @@
 package com.company;
 
 public class Vigenere {
+
     public static String encoder(String word, String key) {
 
         char[][] encryption = new char[26][26];
         char[] alphabet = new char[26];
+
         int index = 0;
         int repeat = 0;
+
         String code = "";
 
         char[] wordChar = word.toCharArray();
@@ -58,9 +61,73 @@ public class Vigenere {
             code += Character.toLowerCase(encryption[y][x]);
         }
 
-        return code;
+        return "Encoder = " + code + " || Key = " + key;
     }
 
+    public static String decoder(String word, String key) {
 
+        char[][] encryption = new char[26][26];
+        char[] alphabet = new char[26];
+
+        int index = 0;
+        int repeat = 0;
+
+        String code = "";
+
+        char[] wordChar = word.toCharArray();
+        char[] keyChar = key.toCharArray();
+
+        int x = 0;
+        int y = 0;
+
+        for (char c = 'a'; c <= 'z'; c++) {
+            alphabet[index] = Character.toUpperCase(c);
+            index++;
+        }
+
+        for (int i = 0; i < encryption.length; i++) {
+            index = repeat;
+            for (int j = 0; j < encryption[i].length; j++) {
+                if (index == 26) {
+                    index = 0;
+                }
+                encryption[i][j] = alphabet[index];
+                index++;
+            }
+            repeat++;
+        }
+
+        index = 0;
+
+        for (int k = 0; k < word.length(); k++) {
+
+            if (index == keyChar.length) {
+                index = 0;
+            }
+
+            if (wordChar[k] == ' ') {
+                code += ' ';
+                continue;
+            }
+
+            for (int l = 0; l < alphabet.length; l++) {
+                if (alphabet[l] == Character.toUpperCase(keyChar[index])) {
+                    x = l;
+                    l = alphabet.length;
+                }
+
+            }
+            for (int m = 0; m < alphabet.length; m++) {
+                if (encryption[x][m] == Character.toUpperCase(wordChar[k])) {
+                    y = m;
+                    m = alphabet.length;
+                }
+            }
+            index++;
+            code += Character.toLowerCase(alphabet[y]);
+        }
+
+        return "Decoder = " + code + " || Key = " + key;
+    }
 
 }
